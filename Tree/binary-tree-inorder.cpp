cb26a672544bbd65bca1fc1fd3ddbf53d847c1c3
ashik jatav <aashikjhajiya@gmail.com>
2026-08-13 22:17:32 +0530

    Create binary-tree-inorder.cpp

---
+++ Tree/binary-tree-inorder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    private:
+  void helper(TreeNode* root, vector<int>& res) {
+        if (!root) return;
+        helper(root->left, res);
+        res.push_back(root->val);
+        helper(root->right, res);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+        
+    }
+};
