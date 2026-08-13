77e963e6a605b8cde39046d53690554362256415
ashik jatav <aashikjhajiya@gmail.com>
2026-08-13 21:49:56 +0530

    Implement Tower of Hanoi algorithm in C++

---
+++ Recursion/tower-of-hanoi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 0)
+        return;
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+
+    cout << "Move disk " << n << " from " << source
+         << " to " << destination << endl;
+
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
